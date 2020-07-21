6733ba73d8c8bcad181df8a56aca8f417f99b64f
Muiota <PhilPro2006@yandex.ru>
2020-07-21 12:00:34 +0300

    sendor poll

--- Indesino.ino
+++ Indesino.ino
@@ -3,12 +3,23 @@
 /*
  Indesino project
 */
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+#define PIN_LED_RED  13
+#define PIN_LED_GREEN  12
+#define PIN_BUTTON_BLACK  10
+#define PIN_BUTTON_BLUE  11
+
 bool pictureInvalid = true;
-int PIN_LED_RED = 13;
-int PIN_LED_GREEN = 12;
-int PIN_BUTTON_BLACK = 11;
-int PIN_BUTTON_BLUE = 10;
+int delayCounter = 0;
+bool timerStatus = false;
+int blueButtonCounter = 0;
+int blackButtonCounter = 0;
+bool blackStatus = false;
+bool blueStatus = false;
+bool lastBlackStatus = false;
+bool lastBlueStatus = false;
+
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 
 // the setup function runs once when you press reset or power the board
@@ -25,13 +36,16 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   drawDisplay();
-  digitalWrite(PIN_LED_RED, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(7, HIGH);
-  delay(1000);                       // wait for a second
-  digitalWrite(PIN_LED_RED, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(7, LOW);
-  delay(1000); 
+  buttonPoll();
+  calcLogic();
   
+  delayCounter++;
+  if (delayCounter >= 100)
+  {
+    delayCounter = 0;
+    timerStatus = !timerStatus;
+  }
+  delay(10); 
 }
 
 
@@ -45,9 +59,14 @@ void drawDisplay() {
    u8g2.drawUTF8(2,35,"59");
  
    u8g2.setFont(u8g2_font_unifont_t_symbols);  // https://github.com/olikraus/u8g2/wiki/fntgrpunifont#font-details
-   u8g2.drawUTF8(74,35,"⏰☀☕⏳⏵");  // write something to the internal memory
-   u8g2.drawUTF8(54,35,"◒"); 
+   u8g2.drawUTF8(74,55,"⏰☀☕⏳⏵");  // write something to the internal memory
+   u8g2.drawUTF8(54,55,"◒"); 
   //  u8g2.sendBuffer();
+  
+   u8g2.drawUTF8(18,55,blackStatus?"◎":"○");  
+   u8g2.drawUTF8(2,55,blueStatus?"◎":"○");  
+
+
    
    u8g2.sendBuffer();         // transfer internal memory to the display
 /*for (int i=0; i < 10; i++)
@@ -70,4 +89,31 @@ void drawDisplay() {
 
     pictureInvalid= false;
 }
+}
+void buttonPoll(){
+blackStatus =  digitalRead(PIN_BUTTON_BLACK);
+blueStatus =  digitalRead(PIN_BUTTON_BLUE);
+if (lastBlackStatus != blackStatus)
+{
+  lastBlackStatus = blackStatus;
+  pictureInvalid = true;
+}
+
+if (lastBlueStatus != blueStatus)
+{
+  lastBlueStatus = blueStatus;
+  pictureInvalid = true;
+}
+
+}
+
+void pressBlackButton() {
+
+}
+
+void pressBlueButton() {
+}
+
+void calcLogic() {
+ digitalWrite(PIN_LED_RED, timerStatus? HIGH : LOW);   
 }
